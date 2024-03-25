6c576d255562d5084c7476753fc115b54e368664
Alexey Kushnir <al7787@gmail.com>
2024-03-25 17:51:53 +0200

    change base name

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')! as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename='/vite-movies-ts'>
+    <BrowserRouter basename='/vite-ts-movies'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
